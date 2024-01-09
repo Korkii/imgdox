060ded3e64c6d7cdfab7f3abc2c8ae6f1c28d2f0
Korkii <oryamoryam1@gmail.com>
2024-01-09 22:13:02 +0000

    featuring: Fixed FIND_WORD_REGEX_VALUE constant value

--- src/Constants.py
+++ src/Constants.py
@@ -1,7 +1,7 @@
 from re import escape
 
 
-FIND_WORD_REGEX_VALUE=rescape(word)+r'(?:\w+|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+FIND_WORD_REGEX_VALUE=r'(?:\w+|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 FIND_URL_REGEX_VALUE = r'http[s]?://(?:\w+|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 FIND_EMAIL_REGEX_VALUE = r"^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$"
 FIND_BASE64_REGEX_VALUE = r'^(?:([a-z0-9A-Z+\/]){4})*(?1)(?:(?1)==|(?1){2}=|(?1){3})$'
