c92b16026cc29c33445abcfea9cd70e87a488bf5
Korkii <oryamoryam1@gmail.com>
2024-01-09 12:05:10 +0000

    featuring: Fixed library imports

--- CommandUtils.py
+++ CommandUtils.py
@@ -1,14 +1,13 @@
 import click
 from os import system
-import subprocess
-import re
-
+from subprocess import check_output as check_command_output
+from re import findall
 import Constants
 from imgdox.CommandParser import print_list,print_strings
             
             
 def find_in_out(out: str,word: str) -> list:
-    uls = re.findall(FIND_WORD_REGEX_VALUE, str(out))
+    uls = findall(FIND_WORD_REGEX_VALUE, str(out))
     
     for i in range(len(uls)):
         uls[i]=uls[i].replace("'","").replace("\\n","")
@@ -17,18 +16,18 @@ def find_in_out(out: str,word: str) -> list:
     
     
 def find_url(out: str) -> list:
-    return re.findall(FIND_URL_REGEX_VALUE, str(out))
+    return findall(FIND_URL_REGEX_VALUE, str(out))
             
    
 def find_emails(out: str) -> list:
-    results = re.findall(FIND_EMAIL_REGEX_VALUE, out.decode(), re.MULTILINE)
+    results = findall(FIND_EMAIL_REGEX_VALUE, out.decode(), re.MULTILINE)
     
     for i in range(len(results)):
         results[i] = "".join(results[i])
         
     return results
 def find_base64(out: str) -> list:
-    uls = re.findall(FIND_BASE64_REGEX_VALUE, str(out))
+    uls = findall(FIND_BASE64_REGEX_VALUE, str(out))
     
     for i in range(len(uls)):
         uls[i]=uls[i].replace("'","").replace("\\n","")
@@ -36,19 +35,20 @@ def find_base64(out: str) -> list:
     return uls
             
 def runCmd(cmdName: str, fileName: str) -> bytes:
-    return subprocess.check_output(cmdName+ " " + str(fileName), shell=True)
+    return check_command_output(cmdName+ " " + str(fileName), shell=True)
 
 @click.command()
 @click.option("--s", default="",required=False, help="List of words to look for. ( Seperated by a comma )")
 @click.option("--f", default="", required=True, help="File name.")
+@click.option("--o", default="", required=False, help="Output file.")
 def basic_scan(s, f):
     
     words = s.split(",")
     
     
     # Command Running
-    outStrings = subprocess.check_output("strings -n7 " + str(f), shell=True)
-    outExiftool = subprocess.check_output("exiftool " + str(f),shell=True)
+    outStrings = check_command_output("strings -n7 " + str(f), shell=True)
+    outExiftool = check_command_output("exiftool " + str(f),shell=True)
     
     cat= []
     exiftool = []
@@ -57,7 +57,7 @@ def basic_scan(s, f):
     
     urls = find_url(outStrings) + find_url(outExiftool)
     emails = find_emails(outStrings) + find_emails(outExiftool)
-
+    base64s = find_base64(outStrings) + find_base64(outExiftool)
 
     for i in range(len(urls)):
         urls[i]=urls[i].replace("'","").replace("\\n","")
@@ -67,15 +67,8 @@ def basic_scan(s, f):
             strings.append(find_in_out(str(outStrings),words[i]))
             exiftool.append(find_in_out(str(outExiftool),words[i]))
         
-
-
-    # Print found urls.
-    print_list(urls,"URLs")
-    # If any strings were looked for, print them.
+    
     print_strings(s,"Strings")
-
-
-    click.secho("\nBase 64 found:",fg="green")
-
-
+    print_list(urls,"URLs")
+    print_list(base64s,"Base64 Literals")
     print_list(emails,"Emails")
