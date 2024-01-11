b26dfdef77db82b42c3791716300d8ab51732837
Korkii <oryamoryam1@gmail.com>
2024-01-11 06:30:54 +0000

    featuring: updated test.py

--- test.py
+++ test.py
@@ -1,22 +1,13 @@
+from subprocess import check_output as check_command_output
 import re
-with open("test.txt") as f:
-    c = f.read()
-    
-regex = r"^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$"
-#urls = re.findall(regex, c)
-matches = re.finditer(regex, c, re.MULTILINE)
-matches2 = re.findall(regex,c,re.MULTILINE)
-print(matches)
-print(matches2)
+def runCmd(cmdName: str, fileName: str) -> bytes:
+    return check_command_output(cmdName+ " " + str(fileName), shell=True)
+
+outStrings = check_command_output("cat " + "img.jpg", shell=True)
+print(outStrings)
 
 
-for matchNum, match in enumerate(matches, start=1):
-    
-    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
-    for groupNum in range(0, len(match.groups())):
-        groupNum = groupNum + 1
-        
-        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+uls = re.findall(r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$',str(outStrings))
 
 
+print(uls)
